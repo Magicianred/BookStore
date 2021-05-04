41c2d00c0b10ff36c557ad86d948e286ec48f320
mmustafaseker <mustiseker.06@gmail.com>
2021-05-04 12:58:37 +0300

    basket order entities

---
+++ src/ApplicationCore/Entities/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class OrderItem : BaseEntity
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUri { get; set; }
+    }
+}
